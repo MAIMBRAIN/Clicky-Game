94e7a0846bef78273cf0e69d19aeb217ecf488cb
Avery Stahl <averystahl@hotmail.com>
2021-09-27 11:18:35 -0400

    Edit favicon, cleanup unnecessary files

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './App.css';
 import {Navbar, NavbarBrand, NavbarText, Container, Card, CardImg} from "reactstrap";
 import Pokemon from "./cards.json"
 
